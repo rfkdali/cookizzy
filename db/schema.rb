e84c9c5aa4aed1b763b18165bdfedf7b2ce173ab
Rafik Dali <rfkdali@gmail.com>
2021-12-08 12:21:39 +0100

    Added recipes columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_122801) do
+ActiveRecord::Schema.define(version: 2021_12_08_105728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 2021_12_07_122801) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.tsvector "tsv"
+    t.string "rate"
+    t.string "author_tip"
+    t.string "budget"
+    t.string "prep_time"
+    t.string "author"
+    t.string "difficulty"
+    t.string "people_quantity"
+    t.string "cook_time"
+    t.string "tags"
+    t.string "total_time"
+    t.string "image"
+    t.string "nb_comments"
     t.index ["tsv"], name: "index_recipes_on_tsv", using: :gin
   end
 
